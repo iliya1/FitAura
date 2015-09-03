763dc5bc953cad74e0971fdff30a21a68f0a1b25
Timur Kunayev <timur.kunayev@gmail.com>
2015-09-03 09:12:34 -0600

    added devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,2 @@
 module ApplicationHelper
-
-  def foundation_class_for flash_type
-    case flash_type
-      when :success
-        "success"
-      when :error
-        "alert"
-      when :alert
-        "warning"
-      when :notice
-        "secondary"
-      else
-        flash_type.to_s
-    end
-  end
-
 end
