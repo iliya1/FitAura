6b16fdc9d8aefddaf195b33c08589f7591e22845
Timur Kunayev <timur.kunayev@gmail.com>
2015-09-05 12:19:01 -0600

    updated s3 code

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -7,7 +7,12 @@ class Studio < ActiveRecord::Base
 
   validates :name, :zipcode, presence: true
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :logo,
+    :storage => :s3,
+    s3_permissions: "public-read",
+    path: "/logos/:styles/:id/:filename",
+    styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/logos/:styles/missing.png"
+
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
 
   def to_param
--- config/application.rb
+++ config/application.rb
@@ -32,10 +32,5 @@ module Fitaura
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :bucket => 'fitaura'
-    }
-
   end
 end
