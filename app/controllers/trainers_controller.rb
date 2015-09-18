378dbf3def3bb3bd5f8b09f4b9f53122e1ad1091
Timur Kunayev <timur.kunayev@gmail.com>
2015-09-18 17:37:04 -0600

    added photo to triner profiles

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,9 +1,35 @@
 class TrainersController < ApplicationController
+  before_action :authenticate_trainer!, :only => [:edit, :update, :dashboard]
+  before_action :load_current_trainer, :only => [:edit, :update, :dashboard]
+
   def dashboard
-    @trainer = current_trainer
   end
 
   def show
     @trainer = Trainer.find params[:id]
   end
+
+  def edit
+  end
+
+  def update
+    if @trainer.update_attributes trainer_attributes
+      redirect_to edit_trainer_path(current_trainer)
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def load_current_trainer
+    @trainer = current_trainer
+  end
+
+  private
+
+  def trainer_attributes
+    params.require(:trainer).permit( :photo )
+  end
+
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,4 +3,14 @@ class Trainer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, :zipcode, presence: true
+
+  has_attached_file :photo,
+    :storage => :s3,
+    s3_permissions: "public-read",
+    path: "/photos/:styles/:id/:filename",
+    styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/photos/:styles/missing.png"
+
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
