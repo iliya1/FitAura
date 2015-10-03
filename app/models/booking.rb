25c03ef4fa1d24bd7be70dea959e09804d45ac51
Timur Kunayev <timur.kunayev@gmail.com>
2015-10-02 20:14:32 -0600

    fixed uniqueness validation on 3 fields

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ActiveRecord::Base
   belongs_to :timeslot
 
   validates :timeslot_id, :booking_date, presence: true
-  #validates :uniquness, :time
+  validates :timeslot_id, uniqueness: { :scope => [:user_id, :booking_date] }
 end
