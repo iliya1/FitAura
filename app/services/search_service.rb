95c003e70e4a69a5ba14490d75274cbb8d6598c4
Timur Kunayev <timur.kunayev@gmail.com>
2015-10-11 11:07:00 -0600

    enabled search

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,12 +1,15 @@
 class SearchService
 
   def self.classes(zip, query)
-    studios = Studio.where(zipcode: zip).includes(:studio_classes)
-    trainers = Trainer.where(zipcode: zip).includes(:trainer_classes)
+    studios = Studio.where(zipcode: zip)
+    trainers = Trainer.where(zipcode: zip)
 
     if query
+      query = "%#{query}%"
+      StudioClass.where("studio_id in (?) AND ( name ilike ? OR description ilike ?)", studios.collect(&:id), query, query ) + 
+        TrainerClass.where("trainer_id in (?) AND ( name ilike ? OR description ilike ?)", trainers.collect(&:id), query, query )
     else
-      ( studios.collect(&:studio_classes) + trainers.collect(&:trainer_classes) ).flatten
+      StudioClass.where("studio_id in (?)", studios.collect(&:id) ) + TrainerClass.where("trainer_id in (?)", trainers.collect(&:id) )
     end
   end
 
