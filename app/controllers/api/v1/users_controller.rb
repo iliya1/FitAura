55e520941d43c3e842903b2625029030c78ae222
Timur Kunayev <timur.kunayev@gmail.com>
2015-11-16 21:35:19 -0700

    added user auth api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,29 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      
+      acts_as_token_authentication_handler_for User, fallback: :none, :except=>[:create,:show]
+      protect_from_forgery :except => [:create, :show]
+
+      def create
+        @user = User.new registration_params
+        if @user.valid?
+          @user.save!
+          render json: {id: @user.id, user_token: @user.authentication_token}
+        else
+          render json: {errors: @user.errors.full_messages}
+        end
+
+      end
+
+      def show
+        @user = User.find params[:id]
+        head 500 and return unless @user && @user.valid_password?(params[:password])
+        render json: {id: @user.id, user_token: @user.authentication_token}
+      end
+
+      def registration_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,9 @@ Rails.application.routes.draw do
 
  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-      resources :users
+      resources :users do
+        post :login
+      end
     end
   end
 
